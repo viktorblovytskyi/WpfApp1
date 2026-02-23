118462c19df39c651de711b0960ca02f0641e213
Viktor Blovytskyi <leito3101@gmail.com>
2026-02-23 12:15:56 +0200

    Initialized the base UI structure.

---
+++ DesktopApplication/Controls/SpectrumControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApplication.Controls
+{
+    /// <summary>
+    /// Interaction logic for SpectrumControl.xaml
+    /// </summary>
+    public partial class SpectrumControl : UserControl
+    {
+        public SpectrumControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
